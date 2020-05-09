538a7871e198b5fcac1570b495cf24490ce24870
Fabien Chereau <fabien.chereau@gmail.com>
2020-05-09 15:27:40 +0200

    Add python script to update .po files

---
+++ update_po.py
@@ -0,0 +1,263 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import sys
+try:
+    import ujson as json
+except ImportError:
+    print("Fallback to standard JSON parser")
+    import json
+import os.path
+import re
+import polib
+import html
+import utils
+
+USE_GOOGLE_TANSLATE = False
+if USE_GOOGLE_TANSLATE:
+    from google.cloud import translate_v2 as translate
+    translate_client = translate.Client()
+
+if sys.version_info[0] < 3:
+    raise Exception("Please use Python 3 (current is {})".format(
+        sys.version_info[0]))
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Sky cultures with clean descriptions ready to be shipped in SWE
+CLEAN_SKYCULTURES = ['belarusian', 'arabic', 'aztec']
+
+# Generate the list of all know common names (in english)
+all_english_cn = []
+for id, name_data in utils.COMMON_NAMES.items():
+    for nd in name_data:
+        all_english_cn.append(nd['english'])
+all_english_cn = set(all_english_cn)
+
+
+# Generate a po translation file for the given sky culture.
+# legacy is a dict containing existing translations to merge in this po.
+def po_for_skyculture(sc, lang, team, legacy):
+    po = polib.POFile(encoding='utf-8', check_for_duplicates=True)
+    po.metadata = {
+        'Project-Id-Version': '1.0',
+        'Last-Translator': "Stellarium's developers",
+        'Language-Team': team,
+        'MIME-Version': '1.0',
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Content-Transfer-Encoding': '8bit',
+        'Language': lang
+    }
+
+    # Sky Culture title
+    entry = polib.POEntry(msgid=sc['name'],
+                          msgstr=legacy.get(sc['name'], ''),
+                          tcomment="Sky culture name")
+    po.append(entry)
+
+    # Sky culture description elements
+    for key in utils.SKY_CULTURE_MD_KEYSL:
+        entry = polib.POEntry(msgid=sc[key],
+                              msgstr=legacy.get(sc[key], ''),
+                              tcomment='Sky culture ' + key +
+                              ' section in markdown format')
+        try:
+            if sc[key]:
+                po.append(entry)
+        except Exception as e:
+            print('Error while adding entry for ' + key + ' in sky culture ' +
+                  sc['id'])
+            print('msgid=' + sc[key] + ' msgstr=' + legacy.get(sc[key], ''))
+            raise e
+
+    # Constellation names & descriptions
+    prefer_native = False
+    if 'langs_use_native_names' in sc:
+        # Use the native name as a translation reference string if english
+        # is listed as a language using native name. E.g. in the case of
+        # the Western sky culture, we want to use "Aquila" as a translatable
+        # string for the constellation rather than "Eagle" for most language.
+        # The only exception to this rule is for languages such as spanish
+        # which also use the latin name as a main constellation name and
+        # the translation of the english name as a secondary name. In such case
+        # we don't want to translate the latin name (which is not changed) but
+        # only the english name.
+        prefer_native = ('en' in sc['langs_use_native_names'] and
+                         lang not in sc['langs_use_native_names'])
+
+    is_native_lang = False
+    if 'native_lang' in sc:
+        is_native_lang = lang == sc['native_lang']
+
+    for constel in sc['constellations']:
+        id = constel['id']
+        notes_id = ''
+        if 'common_name' in constel:
+            cn = constel['common_name']
+            if prefer_native and 'native' in cn:
+                english = cn['native']
+                if notes_id == '':
+                    notes_id = english
+            elif 'english' in cn:
+                english = cn['english']
+                if notes_id == '':
+                    notes_id = english
+            # Don't add names in po if it already exist in the
+            # international common names
+            if english in all_english_cn:
+                english = ''
+            if english:
+                tr = legacy.get(english, '')
+                if is_native_lang and 'native' in cn:
+                    tr = cn['native']
+                notes = sc['name'] + ' constellation'
+                if 'native' in cn:
+                    notes = notes + ', native: ' + cn['native']
+                if 'pronounce' in cn:
+                    notes = notes + ', pronounce: ' + cn['pronounce']
+                entry = polib.POEntry(msgid=english, msgstr=tr, tcomment=notes)
+                if entry not in po:
+                    po.append(entry)
+        if 'description' in constel:
+            desc = constel['description']
+            tr = legacy.get(desc, '')
+            if notes_id == '':
+                notes_id = id
+            notes = 'Description of ' + sc['name'] + ' constellation ' + notes_id
+            entry = polib.POEntry(msgid=desc, msgstr=tr,
+                                  tcomment=notes)
+            if entry not in po:
+                po.append(entry)
+    # Other common names
+    for id, cns in sc.get('common_names', {}).items():
+        for cn in cns:
+            if 'english' in cn:
+                english_name = cn['english']
+            else:
+                continue
+            if sc['id'] in ['chinese', 'chinese_contemporary']:
+                cleaned = english_name
+                cleaned = re.sub(' Added', '', cleaned)
+                has_added = cleaned != english_name
+                cleaned = re.sub(' [MDCLXVI]+$', '', cleaned)
+                cleaned_tr = legacy.get(cleaned, '')
+                notes = 'Chinese star name for ' + id
+                entry = polib.POEntry(msgid=cleaned,
+                                      msgstr=cleaned_tr,
+                                      tcomment=notes)
+                if entry not in po:
+                    po.append(entry)
+
+                if has_added:
+                    notes = '"Added" chinese star name'
+                    added_tr = legacy.get('Added', '')
+                    entry = polib.POEntry(
+                        msgid='Added',
+                        msgstr=added_tr,
+                        tcomment=notes)
+                    if entry not in po:
+                        po.append(entry)
+            else:
+                # Don't add names in po if it already exist in the
+                # international common names
+                if english_name in all_english_cn:
+                    continue
+                tr = legacy.get(english_name, '')
+                if is_native_lang and 'native' in cn:
+                    tr = cn['native']
+                notes = 'Name for ' + id
+                entry = polib.POEntry(msgid=english_name, msgstr=tr,
+                                      tcomment=notes)
+                if entry not in po:
+                    po.append(entry)
+    return po
+
+
+# Translate the passed markdown string using google translate
+def translate_markdown(str, lang):
+    print(str)
+    # Convert [#123] refs to something more google translate-friendly
+    str = re.sub(r'\[#(\d+)\]', '[RefX\\1]', str)
+    str = re.sub(r'\n - ', '\n<lli>', str)
+    str = re.sub(r'^ - ', '<lli>', str)
+    str = str.replace('\n', '<br>\n')
+    tr = translate_client.translate(str, target_language=lang)
+    text = tr['translatedText']
+    text = html.unescape(text)
+    # Re-convert refs to proper format
+    text = re.sub(r' ?<br> ?', '\n', text)
+    text = re.sub(r' ?<lli> ?', ' - ', text)
+    text = re.sub(r'\[RefX(\d+)\]', '[#\\1]', text)
+    # Fix references lists in japanese
+    text = re.sub(r' - \[#(\d+)\]：', ' - [#\\1]: ', text)
+    # Fix references lists in korean
+    text = re.sub(r' - \[#(\d+)\] : ', ' - [#\\1]: ', text)
+    # Fix references lists, google translates seems to drop the new lines..
+    # Fix spaces between links parts [xx] (url) -> [xx](url)
+    text = re.sub(r'( \[.+\]) (\(\S+\))', '\\1\\2', text)
+    text = re.sub(r'( \[.+\])（(\S+)）', '\\1(\\2)', text)
+    # Fix images links extra space
+    text = re.sub(r'! ?(\[.*\]) ?(\(\S+\))', '!\\1\\2', text)
+    # Fix images links extra space (asian version)
+    text = re.sub(r'！(\[.*\]) ?(\(\S+\))', '!\\1\\2', text)
+    text = re.sub(r'！(\[.*\]) ?（(\S+)）', '!\\1(\\2)', text)
+    # Fix extra style content missing space
+    text = re.sub(r'{: (.*)}', '{: \\1 }', text)
+    text = re.sub(r'{：(.*)}', '{: \\1 }', text)
+    print(text)
+    return text
+
+
+# Fill-in all missing translations in the passed po using google translate
+def auto_translate_po(po):
+    lang = po.metadata['Language']
+    for entry in po.untranslated_entries():
+        tr = translate_markdown(entry.msgid, lang)
+        entry.msgstr = tr
+
+
+def main():
+    # Update all po files from the sky culture english content
+    sclist = [d for d in os.listdir(DIR) if os.path.isdir(d)]
+    sclist.sort()
+    for sky_culture in sclist:
+        data_path = os.path.join(DIR, sky_culture)
+        index_file = os.path.join(data_path, 'index.json')
+        if not os.path.exists(index_file):
+            continue
+
+        print('Processing ' + sky_culture)
+        sc = utils.load_skyculture(data_path)
+
+        langs = []
+        for filename in os.listdir(os.path.join(data_path, 'po')):
+            langs.append(filename.replace('.po', ''))
+        for lang in langs:
+            # Load existing translations
+            current_po_path = os.path.join(data_path, 'po', '%s.po' % lang)
+            current_po = None
+            legacy = {}
+            try:
+                current_po = polib.pofile(current_po_path)
+                team = current_po.metadata['Language-Team']
+                for entry in current_po.translated_entries():
+                    legacy[entry.msgid] = entry.msgstr
+            except:
+                print('Error while loading po file for language ' + lang)
+
+            # Generate a brand new po file from sky culture content, and
+            # merge existing translations.
+            po_md = po_for_skyculture(sc, lang, team, legacy)
+
+            # Complete missing translations using Google Translate service
+            if (USE_GOOGLE_TANSLATE
+                    and lang in utils.OFFICIAL_LANGS
+                    and sc['id'] in CLEAN_SKYCULTURES):
+                auto_translate_po(po_md)
+
+            # Replace the current source po file
+            po_md.save(current_po_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import os
+import re
+import json
+import copy
+import uuid
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Languages officially supported by SWE
+OFFICIAL_LANGS = ['ar', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pl', 'pt', 'ru',
+                  'zh_TW', 'zh_CN', 'cs', 'nl']
+
+
+def get_common_names():
+    res = {}
+    current_id = ''
+    current_ninfo = None
+    with open(DIR + '/common_names.tab', 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            fields = [ff.strip() for ff in line.strip().split('|')]
+            ninfo = {
+                'english': fields[1]
+            }
+            if fields[2]:
+                ninfo['wikipedia_title'] = fields[2]
+            if fields[3]:
+                ninfo['model'] = fields[3]
+            if fields[4]:
+                ninfo['sources'] = fields[4].split(',')
+            if fields[5]:
+                ninfo['notes'] = fields[5]
+
+            if fields[0] == current_id:
+                current_ninfo.append(ninfo)
+            else:
+                if current_ninfo:
+                    # Save previously read names
+                    res[current_id] = current_ninfo
+                current_ninfo = [ninfo]
+                current_id = fields[0]
+        # Save last read names
+        res[current_id] = current_ninfo
+    return res
+
+
+COMMON_NAMES = get_common_names()
+COMMON_NAMES_KEYS = set(COMMON_NAMES.keys())
+
+
+def dumps_sky_culture_json(sc):
+    class NoIndent(object):
+        def __init__(self, value):
+            self.value = value
+
+        def append(self, v):
+            self.value.append(v)
+
+    class NoIndentEncoder(json.JSONEncoder):
+        '''JSON encoder that doesn't indent NoIndent values
+           See: https://stackoverflow.com/a/13249927/355230'''
+
+        def __init__(self, *args, **kwargs):
+            super(NoIndentEncoder, self).__init__(*args, **kwargs)
+            self.kwargs = dict(kwargs)
+            del self.kwargs['indent']
+            self._replacement_map = {}
+
+        def default(self, o):
+            if isinstance(o, NoIndent):
+                key = uuid.uuid4().hex
+                self._replacement_map[key] = json.dumps(o.value, **self.kwargs)
+                return "@@%s@@" % (key,)
+            else:
+                return super(NoIndentEncoder, self).default(o)
+
+        def encode(self, o):
+            result = super(NoIndentEncoder, self).encode(o)
+            for k, v in self._replacement_map.items():
+                result = result.replace('"@@%s@@"' % (k,), v)
+            return result
+
+    sc = copy.deepcopy(sc)
+    if 'langs_use_native_names' in sc:
+        sc['langs_use_native_names'] = NoIndent(sc['langs_use_native_names'])
+    if 'common_names' in sc:
+        cn2 = {}
+        for k, val in sc['common_names'].items():
+            cn2[k] = NoIndent(val)
+        sc['common_names'] = cn2
+
+    for feature in sc['constellations']:
+        if 'lines' in feature:
+            feature['lines'] = NoIndent(feature['lines'])
+        if 'image' in feature:
+            feature['image']['anchors'] = [NoIndent(x) for x in
+                                           feature['image']['anchors']]
+            feature['image']['size'] = NoIndent(feature['image']['size'])
+        if 'common_name' in feature:
+            feature['common_name'] = NoIndent(feature['common_name'])
+
+    return json.dumps(sc, indent=2, ensure_ascii=False, cls=NoIndentEncoder)
+
+
+SKY_CULTURE_MD_KEYS = ['Introduction', 'Description', 'References',
+                       'Authors', 'Licence']
+SKY_CULTURE_MD_KEYSL = [k.lower() for k in SKY_CULTURE_MD_KEYS]
+
+
+# Parse a sky culture markdown description file
+def parse_skyculture_markdown(description_file):
+    def feature_text_to_dict(text):
+        ret = {}
+        desc = ''
+        for line in iter(text.splitlines()):
+            m = re.match(r'^ *- *(english|pronounce|native):(.*)$', line)
+            if m:
+                ret[m.group(1)] = m.group(2).strip()
+            else:
+                desc += line + '\n'
+        if desc:
+            ret['description'] = desc.strip()
+        return ret
+
+    ret = {}
+    with open(description_file, 'r', encoding='utf8') as f:
+        sc = {}
+        # Read name from markdown
+        md = f.readline()
+        m = re.match(r'^#\s*(.+)$', md, re.MULTILINE)
+        if not m:
+            print('Error in sky culture ' + sc['id'] +
+                  ': md file must start with # Sky Culture Name')
+        ret['name'] = m.group(1)
+
+        # Read all other sections
+        md = f.read()
+        m = re.split(r'^##\s+(.*)\n', md, 0, re.MULTILINE)
+        sections = {}
+        section_title = ''
+        if m[0] == '':
+            m.pop(0)
+        for s in m:
+            if section_title:
+                sections[section_title] = s.strip('\n')
+                section_title = ''
+            else:
+                section_title = s.strip()
+
+        if 'Constellations' in sections:
+            constellations_text = sections['Constellations']
+            del sections['Constellations']
+
+            m = re.split(r'^#####\s+(.*)\n', constellations_text, 0,
+                re.MULTILINE)
+            features = {}
+            feature_id = ''
+            if m[0] == '':
+                m.pop(0)
+            for s in m:
+                s = s.strip()
+                if feature_id:
+                    features[feature_id] = s
+                    feature_id = ''
+                else:
+                    feature_id = s
+            celestial_objects = {}
+            for (id, text) in features.items():
+                celestial_objects[id] = feature_text_to_dict(text)
+            ret['constellations'] = celestial_objects
+
+        for key in sections.keys():
+            if key not in SKY_CULTURE_MD_KEYS:
+                print('Error in sky culture description, forbidden section: ' +
+                      key)
+                print('Only allowed are: ' + ', '.join(SKY_CULTURE_MD_KEYS) +
+                      ' and Constellations')
+                assert(0)
+
+        for key in sections.keys():
+            ret[key.lower()] = sections[key]
+    return ret
+
+
+# Load the sky culture from the given directory
+def load_skyculture(path):
+    def find_constellation(sc, name):
+        for c in sc['constellations']:
+            if c['id'] == name:
+                return c
+            cn = c['common_name']
+            if cn.get('english', '') == name:
+                return c
+            if cn.get('pronounce', '') == name:
+                return c
+            if cn.get('native', '') == name:
+                return c
+        return None
+
+    index_file = os.path.join(path, 'index.json')
+    description_file = os.path.join(path, 'description.md')
+    sc = None
+
+    with open(index_file, 'r', encoding='utf8') as f:
+        sc = json.load(f)
+
+    md_sc = parse_skyculture_markdown(description_file)
+    if 'constellations' in md_sc:
+        md_constellations = md_sc.get('constellations', [])
+        del md_sc['constellations']
+        for md_coid, md_codata in md_constellations.items():
+            co = find_constellation(sc, md_coid)
+            if co:
+                if 'description' in md_codata:
+                    co['description'] = md_codata['description']
+                if 'pronounce' in md_codata:
+                    co['common_name']['pronounce'] = md_codata['pronounce']
+                if 'english' in md_codata:
+                    co['common_name']['english'] = md_codata['english']
+                if 'native' in md_codata:
+                    co['common_name']['native'] = md_codata['native']
+            else:
+                print('Error: cannot find constellation ' + md_coid +
+                      ' in sky culture ' + sc['id'])
+
+    for key in md_sc.keys():
+        sc[key] = md_sc[key]
+
+    return sc
